f7da75b984d0cef27465ca08a05000f63367d84c
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-26 12:20:14 +0000

    Create script class 2: check size type and value

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square():
+    """Represents a square with a fixed size"""
+
+    def __init__(self, size=0):
+        """Initializes the square size"""
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >=0")
+
+        self.__size = size
