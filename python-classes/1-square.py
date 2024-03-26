d23dba01947b4a2e81e11adef42740f1f47ac5da
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-26 12:12:29 +0000

    Create script class 1: Square class with private attribute size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square():
+    """Represents a square with a fixed size"""
+
+    def __init__(self, size):
+        """Initializes the square size"""
+        self.__size = size
