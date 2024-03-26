b34d6747a3f0a84151362db0f1e7772fba321ba8
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-26 14:59:32 +0000

    Create Script class 101: print square instance

---
+++ python-classes/101-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square():
+    """Represents a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the square size and position"""
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Returns the area of the square instance"""
+        return self.size * self.size
+
+    def my_print(self):
+        """Prints the square to stdout"""
+        print(str(self))
+
+    def __str__(self):
+        """Returns a string that draws the square on multiple lines"""
+        if self.size == 0:
+            return ""
+
+        out = ""
+        for _ in range(self.position[1]):
+            out += "\n"
+
+        line = " " * self.position[0] + "#" * self.size
+        for _ in range(self.size):
+            out += "{}\n".format(line)
+
+        return out[:-1]
+
+    @property
+    def size(self):
+        """Property Getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property Setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        """Property Getter for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Property Setter for position"""
+        if (not isinstance(value, tuple)
+                or len(value) != 2
+                or any(not isinstance(x, int) or x < 0 for x in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = value
