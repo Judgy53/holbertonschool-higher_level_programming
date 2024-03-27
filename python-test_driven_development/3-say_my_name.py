96e124541ee9b13379a5b63c2146139ec72ccac7
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-27 10:20:11 +0000

    Create script test-3: say_my_name script with tests

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains one function, say_my_name(first_name, last_name=""). Example:
+    >>> say_my_name("John", "Smith")
+    "My name is John Smith"
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints "My name is <first_name> <last_name>"
+    'first_name' and 'last_name' must be strings
+    'last_name' is an optional argument
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
