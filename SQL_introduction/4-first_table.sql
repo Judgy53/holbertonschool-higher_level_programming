10da5d04afe695a8a65c23fe86e4a4f24dac4227
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-29 12:36:46 +0000

    Project SQL_introduction-4: First working version

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script that creates a table called first_table in the current database in the MySQL server..
+
+-- Create the table
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
