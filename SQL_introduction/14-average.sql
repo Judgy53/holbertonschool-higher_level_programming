3849fa9f13da3b0cdf7fb6d13fab2af1811df664
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-29 13:47:57 +0000

    Project SQL_introduction-14: Change SQL keywords to uppercase

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Script that computes the score average of all records in the table second_table
 
 -- Select average score
-SELECT AVG(score) as average from second_table;
+SELECT AVG(score) AS average FROM second_table;
