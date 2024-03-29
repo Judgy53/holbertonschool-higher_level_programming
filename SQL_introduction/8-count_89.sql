d016d8404a36250a8487072ef2f7f66a8ae24e4b
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-29 13:00:57 +0000

    Project SQL_introduction-8: First working version

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Script that displays the number of records with id = 89 in the table first_table of the current database.
+
+-- Select rows from first_table where id = 89
+SELECT COUNT(*) FROM first_table WHERE id = 89;
