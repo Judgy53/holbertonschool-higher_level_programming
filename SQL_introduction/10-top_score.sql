02fea66073282f7bbb3644769a920ae24337b9c2
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-29 13:16:26 +0000

    Project SQL_introduction-10: First working version

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Script that lists all records of the table second_table of the current database, ordered by score
+
+-- Select all score and name and order by score (bigger first)
+SELECT score, name FROM second_table ORDER BY score DESC
