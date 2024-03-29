9751be3ff24d2caba91ebdff442582ccc7355fa0
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-29 12:44:00 +0000

    Project SQL_introduction-5: First working version

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Script that prints the create table instructions of the table first_table from the current database in the MySQL server..
+
+-- Print the create table instructions
+SHOW CREATE TABLE first_table;
