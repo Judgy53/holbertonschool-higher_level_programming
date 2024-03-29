53bd51faab29976058ad7e1a156bd0a7b369c16f
Judgy53 <alexandre.gaubert94@orange.fr>
2024-03-29 12:55:09 +0000

    Project SQL_introduction-7: First working version

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Script that inserts a new row in the table first_table in the current database.
+
+-- Insert a new row with values (id=89, name=Best School)
+INSERT INTO first_table (id, name) VALUES(89, "Best School");
