c38ae9df661bb3dd88a2020556ddfc55877fa47c
Judgy53 <alexandre.gaubert94@orange.fr>
2024-04-01 09:52:40 +0000

    Project javascript-web_scraping-2: Remove extra space in output

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@
 const request = require('request');
 request(process.argv[2], (err, response, _) => {
   if (err) throw err;
-  console.log('code: ', response && response.statusCode);
+  console.log('code:', response && response.statusCode);
 });
